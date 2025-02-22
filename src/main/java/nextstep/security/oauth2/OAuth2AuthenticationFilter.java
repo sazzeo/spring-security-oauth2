3c03fe7c13020016db55ddfde249d351120dd6e0
jiyeong-dt <imjee02@gmail.com>
2025-02-23 00:06:29 +0900

    feat: oauth2UserDetailsServiceResolver 가 user 정보 가져오도록 변경

--- src/main/java/nextstep/security/oauth2/OAuth2AuthenticationFilter.java
+++ src/main/java/nextstep/security/oauth2/OAuth2AuthenticationFilter.java
@@ -8,20 +8,16 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import nextstep.app.OAuth2RegistrationRepository;
 import nextstep.security.authentication.Authentication;
-import nextstep.security.authentication.UsernamePasswordAuthenticationToken;
+import nextstep.security.authentication.OAuth2UserDetailsAuthenticationToken;
 import nextstep.security.context.HttpSessionSecurityContextRepository;
 import nextstep.security.context.SecurityContext;
 import nextstep.security.context.SecurityContextHolder;
 import nextstep.security.oauth2.userdetails.OAuth2UserDetailsService;
 import nextstep.security.oauth2.userdetails.OAuth2UserDetailsServiceResolver;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.filter.GenericFilterBean;
 
 import java.io.IOException;
-import java.util.Map;
+import java.util.List;
 import java.util.Set;
 
 public class OAuth2AuthenticationFilter extends GenericFilterBean {
@@ -31,15 +27,11 @@ public class OAuth2AuthenticationFilter extends GenericFilterBean {
 
     private final OAuth2UserDetailsServiceResolver oauth2UserDetailsServiceResolver;
 
-    private final RestTemplate restTemplate = new RestTemplate();
-
-    public OAuth2AuthenticationFilter(final HttpSessionSecurityContextRepository securityContextRepository,
-                                      final OAuth2RegistrationRepository oAuth2RegistrationRepository,
-                                      final Map<String, OAuth2UserDetailsService> userDetailsServiceMap) {
+    public OAuth2AuthenticationFilter(final HttpSessionSecurityContextRepository securityContextRepository, final OAuth2RegistrationRepository oAuth2RegistrationRepository, final List<OAuth2UserDetailsService> userDetailsServices) {
         this.securityContextRepository = securityContextRepository;
         this.oAuth2RegistrationRepository = oAuth2RegistrationRepository;
         this.oAuth2AccessTokenRequestProvider = new OAuth2AccessTokenRequestProviderImpl();
-        this.oauth2UserDetailsServiceResolver = new OAuth2UserDetailsServiceResolver(userDetailsServiceMap);
+        this.oauth2UserDetailsServiceResolver = new OAuth2UserDetailsServiceResolver(userDetailsServices);
     }
 
     @Override
@@ -58,28 +50,15 @@ public class OAuth2AuthenticationFilter extends GenericFilterBean {
         }
         var accessToken = oAuth2AccessTokenRequestProvider.getAccessToken(registration, code);
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Authorization", "Bearer " + accessToken);
-        HttpEntity<Void> httpEntity = new HttpEntity<>(headers);
+        var oauth2DetailsService = oauth2UserDetailsServiceResolver.getService(registration.getVendor());
 
-        var userResponse = restTemplate
-                .exchange("https://api.github.com/user", HttpMethod.GET, httpEntity, Map.class)
-                .getBody();
+        var userDetails = oauth2DetailsService.loadUserByAccessToken(accessToken);
 
-        var email = (String) userResponse.get("email");
-        var name = userResponse.get("name");
-        var avatarUrl = userResponse.get("avatar_url");
-
-        Authentication usernamePasswordToken = UsernamePasswordAuthenticationToken.authenticated(
-                email,
-                accessToken,
-                Set.of("USER")
-        );
+        Authentication usernamePasswordToken = OAuth2UserDetailsAuthenticationToken.authenticated(userDetails.getUsername(), accessToken, Set.of("USER"));
 
         SecurityContext context = new SecurityContext(usernamePasswordToken);
         SecurityContextHolder.setContext(context);
         securityContextRepository.saveContext(context, httpServletRequest, httpServletResponse);
-        filterChain.doFilter(servletRequest, servletResponse);
-
+        httpServletResponse.sendRedirect("/");
     }
 }
