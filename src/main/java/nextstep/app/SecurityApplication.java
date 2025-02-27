fbe5e76f30a2bbe467b055d3527bb4b1541c998b
im.joy <im.joy@dev-lane.com>
2025-02-27 17:32:59 +0900

    refactor: OAuth2Properties -> OAuth2ClientProperties 로 이름 변경
    (cherry picked from commit 6c906bd3e2551ecc90d9b2ddf241e3d2c58241a0)

--- src/main/java/nextstep/app/SecurityApplication.java
+++ src/main/java/nextstep/app/SecurityApplication.java
@@ -1,13 +1,13 @@
 package nextstep.app;
 
-import nextstep.security.properties.OAuth2Properties;
+import nextstep.security.properties.OAuth2ClientProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 
 @SpringBootApplication
-@EnableConfigurationProperties(OAuth2Properties.class)
+@EnableConfigurationProperties(OAuth2ClientProperties.class)
 public class SecurityApplication {
 
     public static void main(String[] args) {
